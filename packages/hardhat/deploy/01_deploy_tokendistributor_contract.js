152d2a2a1f01f504e7ae6a68f78436f3af9abe34
Jaxcoder <codenamejason@outlook.com>
2021-08-26 22:58:37 -0400

    left my address...

--- packages/hardhat/deploy/01_deploy_tokendistributor_contract.js
+++ packages/hardhat/deploy/01_deploy_tokendistributor_contract.js
@@ -3,7 +3,7 @@
 const { ethers } = require("hardhat");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const frontendAddress = "0x3f15B8c6F9939879Cb030D6dd935348E57109637";
+  const frontendAddress = "YOUR_FRONTEND_ADDRESS";
 
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
