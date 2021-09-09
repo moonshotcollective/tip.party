363f0d7a5b7f45620d2e71c21e417ddce1e3828d
Bliss Da Sailor <meremezeukah@gmail.com>
2021-09-09 09:23:54 -0500

    Tipsta setup

--- packages/hardhat/deploy/01_deploy_tokendistributor_contract.js
+++ packages/hardhat/deploy/01_deploy_tokendistributor_contract.js
@@ -5,6 +5,7 @@ const { ethers } = require("hardhat");
 module.exports = async ({ getNamedAccounts, getChainId, deployments }) => {
   // const frontendAddress = "YOUR_FRONTEND_ADDRESS";
   const frontendAddress = process.env.FRONTENDADDRESS;
+  const receiverAddress = process.env.RECEIVERADDRESS;
 
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -20,8 +21,34 @@ module.exports = async ({ getNamedAccounts, getChainId, deployments }) => {
     deployer
   );
 
+  // deploy Tipsta
+  await deploy("Tipsta", {
+    from: deployer,
+    args: [
+      tokenDistributorContract.address,
+      receiverAddress,
+      "100000000000000000",
+    ],
+    log: true,
+  });
+
+  const tipstaContract = await ethers.getContract("Tipsta", deployer);
+  const adminRole = await tokenDistributorContract.DEFAULT_ADMIN_ROLE();
+
+  // give admin role to Tipsta - Tipsta can add new distributors
+  await tokenDistributorContract.grantRole(adminRole, tipstaContract.address);
+
   // run this if not for production deployment
   if (chainId !== 1) {
+    // send test ETH to developer address on localhost
+    if (chainId === 31337) {
+      const deployerWallet = ethers.provider.getSigner();
+      await deployerWallet.sendTransaction({
+        to: receiverAddress,
+        value: ethers.utils.parseEther("1"),
+      });
+    }
+
     await deploy("DummyToken", {
       from: deployer,
       log: true,
@@ -31,7 +58,6 @@ module.exports = async ({ getNamedAccounts, getChainId, deployments }) => {
 
     // transfer ownership to UI owner if needed
     await tokenDistributorContract.transferOwnership(frontendAddress);
-    console.log(`Owned`);
 
     const mintedBalance = await dummyTokenContract.balanceOf(deployer);
     const splitValue = mintedBalance.div(ethers.BigNumber.from(2));
@@ -56,4 +82,4 @@ module.exports = async ({ getNamedAccounts, getChainId, deployments }) => {
   }
 };
 
-module.exports.tags = ["TokenDistributor", "DummyToken"];
+module.exports.tags = ["TokenDistributor", "DummyToken", "Tipsta"];
