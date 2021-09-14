1a4de3fdb2b13cbdee5c4b5897c7c5b8c1592740
Bliss Da Sailor <meremezeukah@gmail.com>
2021-09-13 21:05:35 -0500

    firebase functions

---
+++ packages/backend/functions/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["plugin:prettier/recommended"],
+  rules: {
+    quotes: ["error", "double"],
+  },
+};
---
+++ packages/backend/functions/index.js
@@ -0,0 +1,16 @@
+// const functions = require("firebase-functions");
+const handlers = require("./handlers");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.signRoom = handlers.signRoom;
+exports.addRoomTx = handlers.addRoomTx;
