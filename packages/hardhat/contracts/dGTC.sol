aa71b21daa59960da51cdcb6bcc7a7292db7dcf9
Bliss Da Sailor <meremezeukah@gmail.com>
2021-09-20 21:35:55 -0500

    change dummyToken to dGTC

--- packages/hardhat/contracts/dGTC.sol
+++ packages/hardhat/contracts/dGTC.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // todo: Do we not need any other functionality here? JR
 
 // dummyToken Contract to test out TokenDistribution locally
-contract DummyToken is ERC20 {
-    constructor() public ERC20("DummyToken", "DTKN") {
+contract dGTC is ERC20 {
+    constructor() public ERC20("dummyGTC", "dGTC") {
         _mint(msg.sender, 10000000e18);
     }
 
