16c70a1cd63aaace8a2159314baf03dcefb7e52d
Varun <varunv5000@gmail.com>
2022-02-14 14:58:39 -0500

    Shows a message if the user has not signed into MetaMask

--- packages/react-app/src/components/Account/index.jsx
+++ packages/react-app/src/components/Account/index.jsx
@@ -96,7 +96,7 @@ export default function Account({
               {networkDisplay}
             </div>
           ) : (
-            ""
+            <p>You need to sign into Metamask!</p>
           )}
         </div>,
       );
